bcc3406902c58cfc21844ed7e7914d9b5a5af986
leticiarafaela <leticia.batista.silva24@escola.pr.gov.br>
2026-07-28 17:31:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -23,17 +23,45 @@ function aumentaTamanho(){
    }
 
    const campoSenha = document.querySelector('#campo-senha');
+    const campoSenha = document.querySelectorAll('.checkbox');
+
+
+
+    for (i=0; i <checkbox.length;i++){
+      checkbox[i].onclick = geraSenha;
+    }
+
 
    const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+   const numeros = '0123456789';
+   const símbolos = '!@%*#&?'
    geraSenha();
 
 
    function geraSenha(){
+      let alfabeto = '' ;
+      if (checkbox[0],checked){
+         alfabeto = alfabeto + letrasMaiusculas;
+      }
+
+      if (checkbox[1],checked){
+         alfabeto = alfabeto + letrasMinusculas;
+      }
+
+      if (checkbox[2],checked){
+         alfabeto = alfabeto + numeros;
+      }
+
+      if (checkbox[3],checked){
+         alfabeto = alfabeto + símbolos;
+      }
+
       let senha = '';
       for (let i=0; i < tamanhoSenha;i++) {
-     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+     let numeroAleatorio = Math.random()*alfabeto.length;
      numeroAleatorio = Math.floor(numeroAleatorio);
-     senha = senha + letrasMaiusculas[numeroAleatorio]
+     senha = senha + alfabeto[numeroAleatorio]
    }
    campoSenha.value = senha;
 }
